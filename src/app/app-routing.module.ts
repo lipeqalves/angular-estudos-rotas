33f5666779961fc0ce1ed6ec89e3c1900bdf0ecc
Filipe Alves <lipeqa@gmail.com>
2023-04-22 09:36:59 -0300

    Rotas coringa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,15 @@ import { CardComponent } from './pages/portifolio/card/card.component';
 
 const routes: Routes = [
 {path:'',component:TitleComponent, pathMatch:'full'},
-{path:'portfolio', component: CardComponent, pathMatch:'prefix'}
+{path:'portfolio', component: CardComponent, pathMatch:'prefix'},
+{path:'**',redirectTo:''}
 ];
-
+/**
+ * redirectTo
+ * rota coringa, caso o usuario digite uma rota que não existe,
+ *  ele redireciona para uma pagina de não encontrado
+ *  ou como nesse exemplo para a pagina inicial
+ */
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
